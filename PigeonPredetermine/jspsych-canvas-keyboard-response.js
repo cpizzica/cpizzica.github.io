db3fa763c606e42d41295557968c6a8621bedb85
Chris Pizzica <cpizzica@gmail.com>
2020-09-24 10:43:01 -0400

    6 version update

--- PigeonPredetermine/jspsych-canvas-keyboard-response.js
+++ PigeonPredetermine/jspsych-canvas-keyboard-response.js
@@ -65,27 +65,22 @@ jsPsych.plugins["canvas-keyboard-response"] = (function() {
       },
     }
   }
-
-
+var ScreenWidth = window.innerWidth
+console.log(ScreenWidth)
 
   plugin.trial = function(display_element, trial) {
     // display stimulus
-	  var html = '<canvas id="jspsych-canvas-keyboard-response-stimulus" width="1600" height="800" style="border:1px solid;">' + trial.stimulus + '</canvas>';
-	  html += '<canvas id="second-canvas" width="1600" height="200" style="border:1px solid;"></canvas>'
-	  //html += script_picker + '<br>'
-	  //html += mean_picker + '<br>'
-	  //html += '<br>Frames Left:' + (40-rw)
-	  //html +=`<div class="container" style="height:100px;width:800px;display:flex;flex-direction:column;justify-content:right;align-items:right;text-align:right;font-weight:normal;font-family:Arial;font-size:20px"> Point Bonus ${PointBonus}  <div>`
+	  var html = centered_message(`${SpaceBarMessage}`)
+	  html += '<canvas id="jspsych-canvas-keyboard-response-stimulus">' + trial.stimulus + '</canvas>';
+      html += centered_message(`Steps Remaining: ${AbsoluteFrames}`)
+	  html += centered_message(`Last Response: ${ChoiceDirection}`)
+	  html += centered_message(`Seeds: ${PointTotal}`)
+	  
 	  switch(pre_trials){
 	  case 'start':
-	  
-	 html += '<button onclick="myFunctionWiden()">Accuracy</button> '
-	  html += '<button onclick="myFunctionNarrow()">Speed</button> '
-	  html += '<button onclick="myFunctionBegin()">Begin Trial</button>'
-	  html += `${timeleft}`
+      html += ''
 		  break;
 	  }
-	  console.log(pre_trials === 'start')
 	  	  
 
     // add prompt
@@ -104,34 +99,15 @@ jsPsych.plugins["canvas-keyboard-response"] = (function() {
       rt: null,
       key: null
     };
-      
+//var myCanvas = new fabric.Canvas('myCanvas', { width: 900, height: 600 });
+
+
 var ctx = document.getElementById('jspsych-canvas-keyboard-response-stimulus')
-var canvas = new fabric.Canvas('jspsych-canvas-keyboard-response-stimulus');
+var canvas = new fabric.StaticCanvas('jspsych-canvas-keyboard-response-stimulus', { objectCaching: false, width: trial.stimulus_width, height: 800});
 var canvas2 = new fabric.Canvas('second-canvas');  
-var imgElement = document.getElementById('my-image');
-var imgInstance = new fabric.Image(imgElement, {
-  left: 0,
-  top: 310,
-  angle: 0,
-	opacity: 1
-});
-//canvas.add(imgInstance);  
+ 
       
-// THIS IS WHERE THE LINES ARE DRAW- XSTART,YSTART,XEND,YEND
-
-/*
-------Flipped base-------
-Start: [350. 350.]
-End: [450. 350.]
-*/
-
-
-/*
------Unflipped vertical-------
-Start: [400. 350.]
-End: [400. 250.]
-*/
-vertical_top = new fabric.Line([0,400,1600,400],{
+mid_line = new fabric.Line([0,400,trial.stimulus_width,400],{
     strokeWidth: 2,
     fill: 'black',
     stroke: 'black',
@@ -139,44 +115,35 @@ vertical_top = new fabric.Line([0,400,1600,400],{
     originY : 'center'
   });
 
-
   
-mid_line =  new fabric.Line([750,350,750,450],{
-    strokeWidth: 2,
-    fill: 'black',
-    stroke: 'black',
-    originX : 'center',
-    originY : 'center'
-  }); 
-  
-  UpperChoiceBox = new fabric.Rect({ width: 1600, height: 5, fill: 'red', top: UpperChoiceThreshold, left: 0 });
-  LowerChoiceBox = new fabric.Rect({ width: 1600, height: 5, fill: 'red', top: LowerChoiceThreshold, left: 0 });
+  UpperChoiceBox = new fabric.Rect({objectCaching: false, width: trial.stimulus_width, height: 5, fill: 'red', top: UpperChoiceThreshold, left: 0 });
+  LowerChoiceBox = new fabric.Rect({objectCaching: false, width: trial.stimulus_width, height: 5, fill: 'red', top: LowerChoiceThreshold, left: 0 });
   
   
 var ball_size = 8
 
-height_con = 75
+width_con = (trial.stimulus_width/20)
   
-Circle1 = new fabric.Circle({radius: ball_size, fill: '#f55', opacity: 0.1,top: trial.data[rw - 19], left: height_con * 1 })
-Circle2 = new fabric.Circle({radius: ball_size, fill: '#f55', opacity: 0.1,top: trial.data[rw - 18], left: height_con * 2 })
-Circle3 = new fabric.Circle({radius: ball_size, fill: '#f55', opacity: 0.2,top: trial.data[rw - 17], left: height_con * 3 })
-Circle4 = new fabric.Circle({radius: ball_size, fill: '#f55', opacity: 0.2,top: trial.data[rw - 16], left: height_con * 4 })
-Circle5 = new fabric.Circle({radius: ball_size, fill: '#f55', opacity: 0.3,top: trial.data[rw - 15], left: height_con * 5 })
-Circle6 = new fabric.Circle({radius: ball_size, fill: '#f55', opacity: 0.3,top: trial.data[rw - 14], left: height_con * 6 })
-Circle7 = new fabric.Circle({radius: ball_size, fill: '#f55', opacity: 0.4,top: trial.data[rw - 13], left: height_con * 7 })
-Circle8 = new fabric.Circle({radius: ball_size, fill: '#f55', opacity: 0.4,top: trial.data[rw - 12], left: height_con * 8 })
-Circle9 = new fabric.Circle({radius: ball_size, fill: '#f55', opacity:  0.5,top: trial.data[rw - 11], left: height_con * 9 })
-Circle10 = new fabric.Circle({radius: ball_size, fill: '#f55', opacity: 0.5,top: trial.data[rw - 10], left: height_con * 10 })
-Circle11 = new fabric.Circle({radius: ball_size, fill: '#f55', opacity: 0.6,top: trial.data[rw - 9], left: height_con * 11 })
-Circle12 = new fabric.Circle({radius: ball_size, fill: '#f55', opacity: 0.6,top: trial.data[rw - 8], left: height_con * 12 })
-Circle13 = new fabric.Circle({radius: ball_size, fill: '#f55', opacity: 0.7,top: trial.data[rw - 7], left: height_con * 13 })
-Circle14 = new fabric.Circle({radius: ball_size, fill: '#f55', opacity: 0.7,top: trial.data[rw - 6], left: height_con * 14 })
-Circle15 = new fabric.Circle({radius: ball_size, fill: '#f55', opacity: 0.8,top: trial.data[rw - 5], left: height_con * 15 })
-Circle16 = new fabric.Circle({radius: ball_size, fill: '#f55', opacity: 0.8,top: trial.data[rw - 4], left: height_con * 16 })
-Circle17 = new fabric.Circle({radius: ball_size, fill: '#f55', opacity: 0.9,top: trial.data[rw - 3], left: height_con * 17 })
-Circle18 = new fabric.Circle({radius: ball_size, fill: '#f55', opacity: 0.9, top: trial.data[rw - 2], left: height_con * 18 })
-Circle19 = new fabric.Circle({radius: ball_size, fill: '#f55', opacity: 1.0, top: trial.data[rw - 1], left: height_con * 19 })
-Circle20 = new fabric.Circle({radius: ball_size, fill: '#f55', opacity: 1.0, strokeWidth: 2, stroke: "green", top: trial.data[ rw ], left: height_con * 20 })
+Circle1 = new fabric.Circle({objectCaching: false, radius: ball_size, fill: '#f55', opacity: 0.1,top: trial.data[rw - 19], left: width_con * 1 })
+Circle2 = new fabric.Circle({objectCaching: false, radius: ball_size, fill: '#f55', opacity: 0.1,top: trial.data[rw - 18], left: width_con * 2 })
+Circle3 = new fabric.Circle({objectCaching: false, radius: ball_size, fill: '#f55', opacity: 0.2,top: trial.data[rw - 17], left: width_con * 3 })
+Circle4 = new fabric.Circle({objectCaching: false, radius: ball_size, fill: '#f55', opacity: 0.2,top: trial.data[rw - 16], left: width_con * 4 })
+Circle5 = new fabric.Circle({objectCaching: false, radius: ball_size, fill: '#f55', opacity: 0.3,top: trial.data[rw - 15], left: width_con * 5 })
+Circle6 = new fabric.Circle({objectCaching: false, radius: ball_size, fill: '#f55', opacity: 0.3,top: trial.data[rw - 14], left: width_con * 6 })
+Circle7 = new fabric.Circle({objectCaching: false, radius: ball_size, fill: '#f55', opacity: 0.4,top: trial.data[rw - 13], left: width_con * 7 })
+Circle8 = new fabric.Circle({objectCaching: false, radius: ball_size, fill: '#f55', opacity: 0.4,top: trial.data[rw - 12], left: width_con * 8 })
+Circle9 = new fabric.Circle({objectCaching: false, radius: ball_size, fill: '#f55', opacity:  0.5,top: trial.data[rw - 11], left: width_con * 9 })
+Circle10 = new fabric.Circle({objectCaching: false, radius: ball_size, fill: '#f55', opacity: 0.5,top: trial.data[rw - 10], left: width_con * 10 })
+Circle11 = new fabric.Circle({objectCaching: false, radius: ball_size, fill: '#f55', opacity: 0.6,top: trial.data[rw - 9], left: width_con * 11 })
+Circle12 = new fabric.Circle({objectCaching: false, radius: ball_size, fill: '#f55', opacity: 0.6,top: trial.data[rw - 8], left: width_con * 12 })
+Circle13 = new fabric.Circle({objectCaching: false, radius: ball_size, fill: '#f55', opacity: 0.7,top: trial.data[rw - 7], left: width_con * 13 })
+Circle14 = new fabric.Circle({objectCaching: false, radius: ball_size, fill: '#f55', opacity: 0.7,top: trial.data[rw - 6], left: width_con * 14 })
+Circle15 = new fabric.Circle({objectCaching: false, radius: ball_size, fill: '#f55', opacity: 0.8,top: trial.data[rw - 5], left: width_con * 15 })
+Circle16 = new fabric.Circle({objectCaching: false, radius: ball_size, fill: '#f55', opacity: 0.8,top: trial.data[rw - 4], left: width_con * 16 })
+Circle17 = new fabric.Circle({objectCaching: false, radius: ball_size, fill: '#f55', opacity: 0.9,top: trial.data[rw - 3], left: width_con * 17 })
+Circle18 = new fabric.Circle({objectCaching: false, radius: ball_size, fill: '#f55', opacity: 0.9, top: trial.data[rw - 2], left: width_con * 18 })
+Circle19 = new fabric.Circle({objectCaching: false, radius: ball_size, fill: '#f55', opacity: 1.0, top: trial.data[rw - 1], left: width_con * 19 })
+Circle20 = new fabric.Circle({objectCaching: false, radius: ball_size, fill: '#f55', opacity: 1.0, strokeWidth: 2, stroke: "green", top: trial.data[ rw ], left: width_con * 20 })
 
 canvas.add(Circle1)	  
 canvas.add(Circle2)
@@ -198,73 +165,43 @@ canvas.add(Circle17)
 canvas.add(Circle18)
 canvas.add(Circle19)
 canvas.add(Circle20)
-  canvas2.remove(responsefb)
   
 
 
-ShowWall =  new fabric.Rect({ width: 1600, height: 800, fill: 'white', top: 0, left: 0 })
 
-  canvas.add(vertical_top)
+  canvas.add(mid_line)
 //canvas.add(mid_line)
   switch(pre_trials) {
 	  case 'start': canvas.add(UpperChoiceBox);
   					canvas.add(LowerChoiceBox);
 					break;
 				}
-  /*if (AbsoluteFrames <= 0){
-     canvas.add(ShowWall)
-  }
-  */
   
-var line_bottom = new fabric.Path('M 382 495 Q 400, 405, 418, 495', { fill: '', stroke: 'black', objectCaching: false, strokeWidth: 5 });
-line_bottom.selectable = false;
-
-       
-var responseheader = new fabric.Text('Response:', { left: 100, top: 50, shadow: 'rgba(0,0,0,0.3) 5px 5px 5px',   fontFamily: 'Hoefler Text' });
-var responsefb = new fabric.Text(`${ChoiceDirection}`, { left: 300, top: 50,});
-responsefb.set({fill: `${CDcolor}`})
-
-var seedheader = new fabric.Text('Seeds:', { left: 500, top: 50, shadow: 'rgba(0,0,0,0.3) 5px 5px 5px',   fontFamily: 'Hoefler Text' });
-var seedfb = new fabric.Text(`${PointTotal}`, { left: 625, top: 50 });
-
-var stepsheader = new fabric.Text('Steps Remaining:', { left: 900, top: 50, shadow: 'rgba(0,0,0,0.3) 5px 5px 5px',   fontFamily: 'Hoefler Text'});
-var stepsfbD = new fabric.Text(`${AbsoluteFrames}`, {left: 1200, top: 50})
-
-
-canvas2.add(responseheader)
-
-
-canvas2.add(seedheader)
-canvas2.add(seedfb)
-
-canvas2.add(stepsheader)
-canvas2.add(stepsfbD)
 
 if (BallVisible == false){
-canvas.remove(Circle1)
-canvas.remove(Circle2)
-canvas.remove(Circle3)
-canvas.remove(Circle4)
-canvas.remove(Circle5)
-canvas.remove(Circle6)
-canvas.remove(Circle7)
-canvas.remove(Circle8)
-canvas.remove(Circle9)
-canvas.remove(Circle10)
-canvas.remove(Circle11)
-canvas.remove(Circle12)
-canvas.remove(Circle13)
-canvas.remove(Circle14)
-canvas.remove(Circle15)
-canvas.remove(Circle16)
-canvas.remove(Circle17)
-canvas.remove(Circle18)
-canvas.remove(Circle19)
-canvas.remove(Circle20)
-canvas2.add(responsefb)
+canvas.clear
 
   }      
-	  
+/*	
+var after_response = function(info){
+	switch(info.key){
+		case 38: UpperChoiceThreshold += -20
+		LowerChoiceThreshold += 20
+		break;
+		
+		case 40: UpperChoiceThreshold += 20
+		LowerChoiceThreshold += -20
+		break;
+	}
+}
+
+jsPsych.pluginAPI.getKeyboardResponse({
+  callback_function:after_response,
+  valid_responses: ['uparrow','downarrow'],
+  rt_method: 'performance',
+  persist: false
+});	
+*/	  
     // function to end trial when it is time
     var end_trial = function() {
 
@@ -317,6 +254,8 @@ canvas2.add(responsefb)
         allow_held_key: false
       });
     }
+	
+
 
     // hide stimulus if stimulus_duration is set
     if (trial.stimulus_duration !== null) {
